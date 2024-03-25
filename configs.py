43ee9d3970889c21173e928c27d2ade53f63bed7
Luka Secilmis <lukasecilmis@gmail.com>
2024-03-25 17:55:02 +0100

    added celebA with confounding

--- configs.py
+++ configs.py
@@ -3,6 +3,8 @@ import ml_collections
 
 def mnist_experiment():
     config = ml_collections.ConfigDict()
+    config.model = 'mnist'
+    config.num_classes = 10
     config.learning_rate = 0.01
     config.decay_rate = 0.9999
     config.batch_size = 120
@@ -11,4 +13,20 @@ def mnist_experiment():
     config.lambda_core = 0.2  # Conditional variance regularization strength
     config.train_size = 10000  # Size of training set
     config.aug_size = 200  # Number of augmented images
+    return config
+
+
+def celebA_experiment():
+    config = ml_collections.ConfigDict()
+    config.model = 'celebA'
+    config.num_classes = 2
+    config.learning_rate = 0.01
+    config.decay_rate = 0.9999
+    config.batch_size = 120
+    config.num_epochs = 50
+    config.lambda_l2 = 0.001  # L2 regularization strength
+    config.lambda_core = 0.2  # Conditional variance regularization strength
+    config.train_size = 20000  # Size of training set
+    config.test_size = (4000, 1200) # Size of test sets
+    config.prop_glasses = 0.8 # Proportion of men with and women without glasses in training set
     return config
--- train.py
+++ train.py
@@ -23,8 +23,26 @@ import data_handler
 logging.set_verbosity(logging.INFO)
 SEED = 7
 
-class CNN(nn.Module):
-    """Two-layer CNN as outlined in Table C.1."""
+
+class CNN_celebA(nn.Module):
+    """Five-layer CNN as outlined in Table C.1."""
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Conv(features=16, kernel_size=(5, 5), strides=(2, 2))(x)
+        x = nn.leaky_relu(x, negative_slope=0.001)
+        x = nn.Conv(features=32, kernel_size=(5, 5), strides=(2, 2))(x)
+        x = nn.leaky_relu(x, negative_slope=0.001)
+        x = nn.Conv(features=64, kernel_size=(5, 5), strides=(2, 2))(x)
+        x = nn.leaky_relu(x, negative_slope=0.001)
+        x = nn.Conv(features=128, kernel_size=(5, 5), strides=(2, 2))(x)
+        x = nn.leaky_relu(x, negative_slope=0.001)
+        x = x.reshape((x.shape[0], -1))
+        x = nn.Dense(features=2)(x)
+        return x
+
+
+class CNN_mnist(nn.Module):
+    """Three-layer CNN as outlined in Table C.1."""
     @nn.compact
     def __call__(self, x):
         x = nn.Conv(features=16, kernel_size=(5, 5), strides=(2, 2))(x)
@@ -36,7 +54,8 @@ class CNN(nn.Module):
         return x
 
 
-def apply_model(state, images, labels, ids, lambda_l2, lambda_core):
+# @jax.jit: TODO
+def apply_model(state, images, labels, ids, num_classes, lambda_l2, lambda_core):
     """Computes gradients, loss and accuracy for a single batch."""
     def core_penalty(params, logits, ids):
         _, index_inverse, counts = jnp.unique(ids, return_inverse=True, return_counts=True)  # ids = unique_ids[index_inverse]
@@ -61,16 +80,16 @@ def apply_model(state, images, labels, ids, lambda_l2, lambda_core):
             sum_squares_perID = jax.ops.segment_sum(square_diff, index_inverse[mask], num_segments=jnp.max(index_inverse) + 1)
             variance_perID = sum_squares_perID / count_perID[:, None]
             
-            # Mean of the variances per ID
+            # Mean variance
             penalty = jnp.mean(variance_perID[count_perID > 1])
         return penalty
         
     def l2_penalty(params):
         return sum(jnp.sum(jnp.square(param)) for param in jax.tree_util.tree_leaves(params))
-
+ 
     def loss_fn(params):
         logits = state.apply_fn({'params': params}, images)  # forward pass
-        one_hot = jax.nn.one_hot(labels, 10)
+        one_hot = jax.nn.one_hot(labels, num_classes)
         loss = jnp.mean(optax.softmax_cross_entropy(logits=logits, labels=one_hot))
         l2_pen = l2_penalty(params)
         loss += lambda_l2 * l2_pen
@@ -106,15 +125,9 @@ def train_epoch(state, train_ds, config, rng):
     for perm in perms:
         batch_images = train_ds['image'][perm, ...]
         batch_labels = train_ds['label'][perm, ...]
-        
-        # for CoRe penalty
         batch_ids = train_ds['id'][perm, ...]
-        # _, index_inverse, batch_counts = jnp.unique(batch_ids, return_inverse=True, return_counts=True)  # ids = unique_ids[index_inverse]
-        # batch_num_segments = jnp.max(index_inverse) + 1
-        # batch_mask = batch_counts[index_inverse] > 1
-        # masked_inverse = index_inverse[batch_mask]
 
-        grads, loss, accuracy, core_pen = apply_model(state, batch_images, batch_labels, batch_ids, config.lambda_l2, config.lambda_core)
+        grads, loss, accuracy, core_pen = apply_model(state, batch_images, batch_labels, batch_ids, config.num_classes, config.lambda_l2, config.lambda_core)
         state = update_model(state, grads)
         epoch_loss.append(loss)
         epoch_accuracy.append(accuracy)
@@ -126,14 +139,23 @@ def train_epoch(state, train_ds, config, rng):
     return state, train_loss, train_accuracy, core_penalty
 
 
-def create_train_state(rng, learning_rate, decay_rate, decay_steps):
+def create_train_state(rng, learning_rate, decay_rate, decay_steps, model='mnist'):
     """Initialize weights."""
-    cnn = CNN()
-    params = cnn.init(rng, jnp.ones([1, 28, 28, 1]))['params']
-    schedule = optax.exponential_decay(init_value=learning_rate,
-                                       transition_steps=decay_steps,
-                                       decay_rate=decay_rate)
-    tx = optax.adam(schedule)
+    if model == 'mnist':
+        cnn = CNN_mnist()
+        params = cnn.init(rng, jnp.ones([1, 28, 28, 1]))['params']
+        schedule = optax.exponential_decay(init_value=learning_rate,
+                                        transition_steps=decay_steps,
+                                        decay_rate=decay_rate)
+        tx = optax.adam(schedule)
+
+    elif model == 'celebA':
+        cnn = CNN_celebA()
+        params = cnn.init(rng, jnp.ones([1, 64, 64, 3]))['params']
+        schedule = optax.exponential_decay(init_value=learning_rate,
+                                        transition_steps=decay_steps,
+                                        decay_rate=decay_rate)
+        tx = optax.adam(schedule)
     return train_state.TrainState.create(apply_fn=cnn.apply, params=params, tx=tx)
 
 
@@ -145,13 +167,13 @@ def train_and_evaluate(config: ml_collections.ConfigDict, workdir: str, train_ds
 
     rng, init_rng = jax.random.split(rng)
     decay_steps = len(train_ds['image']) // config.batch_size
-    state = create_train_state(init_rng, config.learning_rate, config.decay_rate, decay_steps)
+    state = create_train_state(init_rng, config.learning_rate, config.decay_rate, decay_steps, model=config.model)
 
     for epoch in range(1, config.num_epochs + 1):
         rng, input_rng = jax.random.split(rng)
         state, train_loss, train_accuracy, core_penalty = train_epoch(state, train_ds, config, input_rng)
-        _, _, test1_accuracy, _ = apply_model(state, test1_ds['image'], test1_ds['label'], jnp.arange(len(test1_ds['image'])), config.lambda_l2, config.lambda_core)
-        _, _, test2_accuracy, _ = apply_model(state, test2_ds['image'], test2_ds['label'], jnp.arange(len(test2_ds['image'])), config.lambda_l2, config.lambda_core)
+        _, _, test1_accuracy, _ = apply_model(state, test1_ds['image'], test1_ds['label'], jnp.arange(len(test1_ds['image'])), config.num_classes, config.lambda_l2, config.lambda_core)
+        _, _, test2_accuracy, _ = apply_model(state, test2_ds['image'], test2_ds['label'], jnp.arange(len(test2_ds['image'])), config.num_classes, config.lambda_l2, config.lambda_core)
 
         # Calculate misclassification rates for the last epoch directly
         misclassification_rate_test1 = 1 - test1_accuracy
